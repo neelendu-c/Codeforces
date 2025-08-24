e2825cc7805038b0ba3ca6f3a65ad48c0d971490
neelendu <neelendu.chakarbarti@gmail.com>
2025-08-24 16:50:57 +0530

    71a

--- 71A_Way_Too_Long_Words.cpp
+++ 71A_Way_Too_Long_Words.cpp
@@ -2,8 +2,20 @@
 using namespace std;
 
 int main() {
-    
-    
+    int n; cin>>n;
 
+    for (int i = 0; i < n; i++)
+    {
+        string st; cin>>st;
+        if (st.length() > 10)
+        {
+            cout<<st[0]<<st.length() - 2<<st[st.length()-1];
+        }
+        else
+        {
+            cout<<st;
+        }
+        cout<<endl;
+    }
     return 0;
 }
