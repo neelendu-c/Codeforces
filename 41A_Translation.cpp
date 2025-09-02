a86fd81442f8274dc79448dcddc5e6695ec74d78
neelendu <neelendu.chakarbarti@gmail.com>
2025-09-02 17:40:18 +0530

    implementation, strings

---
+++ 41A_Translation.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+
+    string s;
+    cin >> s;
+    string t;
+    cin >> t;
+
+    reverse(t.begin(), t.end());
+
+    if (t == s)
+    {
+        cout << "YES";
+    }
+    else
+    {
+        cout << "NO";
+    }
+}
