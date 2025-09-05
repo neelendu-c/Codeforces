7d99d311b5087ce0a28c62bccfc182069acec550
neelendu <neelendu.chakarbarti@gmail.com>
2025-09-06 00:50:27 +0530

    greedy, math

---
+++ 1857A_Array_Coloring.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+
+    int t;
+    cin >> t;
+
+    for (int z = 0; z < t; z++)
+    {
+        int n;
+        cin >> n;
+        int a[n];
+        int oddcount = 0;
+        int evencount = 0;
+
+        for (int i = 0; i < n; i++)
+        {
+            cin >> a[i];
+            if (a[i] % 2 != 0)
+            {
+                oddcount++;
+            }
+        }
+
+        if (oddcount % 2 == 0)
+        {
+            cout << "YES" << endl;
+        }
+        else
+        {
+            cout << "NO" << endl;
+        }
+    }
+}
