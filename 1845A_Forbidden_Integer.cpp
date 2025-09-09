e17ab09c304d033b674fb9b337d1eb0585581f40
neelendu <neelendu.chakarbarti@gmail.com>
2025-09-09 21:56:53 +0530

    1845

---
+++ 1845A_Forbidden_Integer.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+ 
+int main(){
+ 
+    int t; cin>>t;
+
+    for (int z = 0; z < t; z++)
+    {
+        int n; cin>>n;
+        int k; cin>>k; // range end
+        int x; cin>>x; // left out int
+
+        
+    }
+    
+ 
+}
