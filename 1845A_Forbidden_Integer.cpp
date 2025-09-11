fab085f593c9044da65df18ff72c9c3bb7787906
neelendu <neelendu.chakarbarti@gmail.com>
2025-09-12 00:29:13 +0530

    constructive algorithms, implementation, math, number theory

--- 1845A_Forbidden_Integer.cpp
+++ 1845A_Forbidden_Integer.cpp
@@ -1,19 +1,78 @@
 #include <bits/stdc++.h>
 using namespace std;
- 
- 
-int main(){
- 
-    int t; cin>>t;
+
+int main()
+{
+
+    int t;
+    cin >> t;
 
     for (int z = 0; z < t; z++)
     {
-        int n; cin>>n;
-        int k; cin>>k; // range end
-        int x; cin>>x; // left out int
+        int n;
+        cin >> n;
+        int k;
+        cin >> k; // range end
+        int x;
+        cin >> x; // left out int
 
-        
+        if (x != 1)
+        {
+            cout << "YES" << endl
+                 << n << endl;
+            for (int i = 0; i < n; i++)
+            {
+                cout << "1 ";
+            }
+            cout << endl;
+        }
+
+        else
+        {
+            if (k == 1)
+            {
+                cout << "NO" << endl;
+            }
+            else if (k == 2)
+            {
+                if (n % 2 == 0)
+                {
+                    cout << "YES" << endl
+                         << n / 2 << endl;
+                    for (int i = 0; i < n / 2; i++)
+                    {
+                        cout << "2 ";
+                    }
+                    cout << endl;
+                }
+                else
+                {
+                    cout << "NO" << endl;
+                }
+            }
+            else
+            {
+                if (n % 2 != 0)
+                {
+                    cout << "YES" << endl
+                         << (n - 3) / 2 + 1 << endl;
+                    for (int i = 0; i < (n - 3) / 2; i++)
+                    {
+                        cout << "2 ";
+                    }
+                    cout << "3" << endl;
+                }
+                else
+                {
+                    cout << "YES" << endl
+                         << n / 2 << endl;
+                    for (int i = 0; i < n / 2; i++)
+                    {
+                        cout << "2 ";
+                    }
+                    cout << endl;
+                }
+            }
+        }
     }
-    
- 
 }
